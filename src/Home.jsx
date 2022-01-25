c28a5ec9f6b7f9fd8bc62905db46553b6803d104
pablo99nunez <pablo99nunez@gmail.com>
2022-01-25 14:07:30 -0300

    add swipe instruction

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,6 +18,7 @@ export default function Home() {
   const countries = useSelector((state) => state.countries);
   const results = useSelector((state) => state.results);
   const [isLoading, setIsLoading] = useState(true);
+ 
   const [page, setPage] = useState(0);
 
   const dispatch = useDispatch();
@@ -62,7 +63,9 @@ export default function Home() {
           >
             No hay resultados
           </h1>
-        )}
+          )
+          
+          }
       </div>
     </div>
   );
